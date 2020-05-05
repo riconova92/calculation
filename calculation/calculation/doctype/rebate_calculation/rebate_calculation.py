9663302444bccb3f3c354874f0a2006dff767bf7
root <root@139.162.1.138>
2020-05-05 17:51:04 +0700

    merevisi rebate calculation and rebate setting

--- calculation/calculation/doctype/rebate_calculation/rebate_calculation.py
+++ calculation/calculation/doctype/rebate_calculation/rebate_calculation.py
@@ -7,7 +7,20 @@ import frappe
 from frappe.model.document import Document
 
 class RebateCalculation(Document):
-	pass
+	
+
+	def validate(self):
+		# do calculation
+		total_amount = 0
+		for i in self.rebate_calculation_child :
+
+			i.column_d = float(i.invoice_amount) * (float(i.column_d_setting) / 100)
+			i.column_e = float(i.column_d) / float(i.column_e_setting)
+			i.amount = float(i.invoice_amount) - float(i.column_d)
+			total_amount = total_amount + float(i.amount)
+
+		self.total_amount = total_amount
+
 
 
 
@@ -17,43 +30,56 @@ def sales_invoice_submit(doc,method):
 	# penambahan comment buat test git
 
 	rebate_setting = frappe.get_single("Rebate Setting")
+	column_e = 0
+	column_d = 0
 
-	# branch
-	customer_id = doc.customer
-	customer_name = doc.name_customer
-	get_rebate = frappe.get_doc("Customer", doc.customer).get_rebate
-	year = str(doc.posting_date).split("-")[0]
-	month_angka = str(doc.posting_date).split("-")[1]
-	month_huruf = ""
+	check_customer_ada = 0
 
-	if month_angka == "01" :
-		month_huruf = "January"
-	elif month_angka == "02" :
-		month_huruf = "February"
-	elif month_angka == "03" :
-		month_huruf = "March"
-	elif month_angka == "04" :
-		month_huruf = "April"
-	elif month_angka == "05" :
-		month_huruf = "May"
-	elif month_angka == "06" :
-		month_huruf = "June"
-	elif month_angka == "07" :
-		month_huruf = "July"
-	elif month_angka == "08" :
-		month_huruf = "August"
-	elif month_angka == "09" :
-		month_huruf = "September"
-	elif month_angka == "10" :
-		month_huruf = "October"
-	elif month_angka == "11" :
-		month_huruf = "November"
-	elif month_angka == "12" :
-		month_huruf = "December"
+	if rebate_setting.rebate_setting_child :
+		for rs in rebate_setting.rebate_setting_child :
+			if rs.customer_id == doc.customer :
 
-	kombinasi_nama = customer_name +" | "+ month_huruf + " " + month_angka
+				check_customer_ada = 1
+				column_e = rs.column_e
+				column_d = rs.column_d
 
-	if get_rebate :
+	if check_customer_ada == 1 :
+
+
+		# branch
+		customer_id = doc.customer
+		customer_name = doc.name_customer
+		
+		year = str(doc.posting_date).split("-")[0]
+		month_angka = str(doc.posting_date).split("-")[1]
+		month_huruf = ""
+
+		if month_angka == "01" :
+			month_huruf = "January"
+		elif month_angka == "02" :
+			month_huruf = "February"
+		elif month_angka == "03" :
+			month_huruf = "March"
+		elif month_angka == "04" :
+			month_huruf = "April"
+		elif month_angka == "05" :
+			month_huruf = "May"
+		elif month_angka == "06" :
+			month_huruf = "June"
+		elif month_angka == "07" :
+			month_huruf = "July"
+		elif month_angka == "08" :
+			month_huruf = "August"
+		elif month_angka == "09" :
+			month_huruf = "September"
+		elif month_angka == "10" :
+			month_huruf = "October"
+		elif month_angka == "11" :
+			month_huruf = "November"
+		elif month_angka == "12" :
+			month_huruf = "December"
+
+		kombinasi_nama = customer_name +" | "+ month_huruf + " " + month_angka
 
 		cek_dulu = frappe.get_value("Rebate Calculation", {"name":kombinasi_nama}, "name")
 
@@ -70,8 +96,8 @@ def sales_invoice_submit(doc,method):
 
 						i.invoice_amount = float(i.invoice_amount) + float(doc.grand_total)
 
-						i.column_d = float(i.invoice_amount) * (float(rebate_setting.column_d) / 100)
-						i.column_e = float(i.column_d) / float(rebate_setting.column_e)
+						i.column_d = float(i.invoice_amount) * (float(column_d) / 100)
+						i.column_e = float(i.column_d) / float(column_e)
 						i.amount = float(i.invoice_amount) - float(i.column_d)
 
 						count = 1
@@ -81,9 +107,14 @@ def sales_invoice_submit(doc,method):
 
 					new_row.invoice_amount = float(doc.grand_total)
 					new_row.branch = doc.customer
-					new_row.column_d = float(new_row.invoice_amount) * (float(rebate_setting.column_d) / 100)
-					new_row.column_e = float(new_row.column_d) / float(rebate_setting.column_e)
+					new_row.column_d_setting = column_d
+					new_row.column_e_setting = column_e
+
+					new_row.column_d = float(new_row.invoice_amount) * (float(new_row.column_d_setting) / 100)
+					new_row.column_e = float(new_row.column_d) / float(new_row.column_e_setting)
 					new_row.amount = float(new_row.invoice_amount) - float(new_row.column_d)
+
+
 
 
 				total_count = 0
@@ -109,8 +140,11 @@ def sales_invoice_submit(doc,method):
 			new_row = new_doc.append("rebate_calculation_child", {})
 			new_row.branch = doc.customer
 			new_row.invoice_amount = float(doc.grand_total)
-			new_row.column_d = float(new_row.invoice_amount) * (float(rebate_setting.column_d) / 100)
-			new_row.column_e = float(new_row.column_d) / float(rebate_setting.column_e)
+			new_row.column_d_setting = column_d
+			new_row.column_e_setting = column_e
+			
+			new_row.column_d = float(new_row.invoice_amount) * (float(new_row.column_d_setting) / 100)
+			new_row.column_e = float(new_row.column_d) / float(new_row.column_e_setting)
 			new_row.amount = float(new_row.invoice_amount) - float(new_row.column_d)
 
 
@@ -127,42 +161,56 @@ def sales_invoice_cancel(doc,method):
 
 	rebate_setting = frappe.get_single("Rebate Setting")
 
-	# branch
-	customer_id = doc.customer
-	customer_name = doc.name_customer
-	get_rebate = frappe.get_doc("Customer", doc.customer).get_rebate
-	year = str(doc.posting_date).split("-")[0]
-	month_angka = str(doc.posting_date).split("-")[1]
-	month_huruf = ""
 
-	if month_angka == "01" :
-		month_huruf = "January"
-	elif month_angka == "02" :
-		month_huruf = "February"
-	elif month_angka == "03" :
-		month_huruf = "March"
-	elif month_angka == "04" :
-		month_huruf = "April"
-	elif month_angka == "05" :
-		month_huruf = "May"
-	elif month_angka == "06" :
-		month_huruf = "June"
-	elif month_angka == "07" :
-		month_huruf = "July"
-	elif month_angka == "08" :
-		month_huruf = "August"
-	elif month_angka == "09" :
-		month_huruf = "September"
-	elif month_angka == "10" :
-		month_huruf = "October"
-	elif month_angka == "11" :
-		month_huruf = "November"
-	elif month_angka == "12" :
-		month_huruf = "December"
+	column_e = 0
+	column_d = 0
 
-	kombinasi_nama = customer_name +" | "+ month_huruf + " " + month_angka
+	check_customer_ada = 0
 
-	if get_rebate :
+	if rebate_setting.rebate_setting_child :
+		for rs in rebate_setting.rebate_setting_child :
+			if rs.customer_id == doc.customer :
+
+				check_customer_ada = 1
+				column_e = rs.column_e
+				column_d = rs.column_d
+
+	if check_customer_ada == 1 :
+
+		# branch
+		customer_id = doc.customer
+		customer_name = doc.name_customer
+		
+		year = str(doc.posting_date).split("-")[0]
+		month_angka = str(doc.posting_date).split("-")[1]
+		month_huruf = ""
+
+		if month_angka == "01" :
+			month_huruf = "January"
+		elif month_angka == "02" :
+			month_huruf = "February"
+		elif month_angka == "03" :
+			month_huruf = "March"
+		elif month_angka == "04" :
+			month_huruf = "April"
+		elif month_angka == "05" :
+			month_huruf = "May"
+		elif month_angka == "06" :
+			month_huruf = "June"
+		elif month_angka == "07" :
+			month_huruf = "July"
+		elif month_angka == "08" :
+			month_huruf = "August"
+		elif month_angka == "09" :
+			month_huruf = "September"
+		elif month_angka == "10" :
+			month_huruf = "October"
+		elif month_angka == "11" :
+			month_huruf = "November"
+		elif month_angka == "12" :
+			month_huruf = "December"
+
+		kombinasi_nama = customer_name +" | "+ month_huruf + " " + month_angka
 
 		cek_dulu = frappe.get_value("Rebate Calculation", {"name":kombinasi_nama}, "name")
 
@@ -179,8 +227,12 @@ def sales_invoice_cancel(doc,method):
 
 						i.invoice_amount = float(i.invoice_amount) - float(doc.grand_total)
 
-						i.column_d = float(i.invoice_amount) * (float(rebate_setting.column_d) / 100)
-						i.column_e = float(i.column_d) / float(rebate_setting.column_e)
+						i.column_d_setting = column_d
+						i.column_e_setting = column_e
+						
+
+						i.column_d = float(i.invoice_amount) * (float(i.column_d_setting) / 100)
+						i.column_e = float(i.column_d) / float(i.column_e_setting)
 						i.amount = float(i.invoice_amount) - float(i.column_d)
 
 						count = 1
